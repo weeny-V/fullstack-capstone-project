0405e5af7a86f44a276c5f93b7f1b782cd6b42fb
weeny-V <vladkruglovfencer@gmail.com>
2026-06-20 15:12:38 -0400

    implement search routes

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -21,12 +21,8 @@ connectToDatabase().then(() => {
 
 app.use(express.json());
 
-// Route files
-// Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
-const giftroutes = require("./routes/giftRoutes")
-
-// Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
-//{{insert code here}}
+const giftroutes = require("./routes/giftRoutes");
+const searchRoutes = require("./routes/searchRoutes");
 
 
 const pinoHttp = require('pino-http');
@@ -36,10 +32,10 @@ app.use(pinoHttp({ logger }));
 
 // Use Routes
 // Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
-app.use('/api/gifts', giftroutes)
+app.use('/api/gifts', giftroutes);
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
-//{{insert code here}}
+app.use('/api/search', searchRoutes);
 
 
 // Global Error Handler
