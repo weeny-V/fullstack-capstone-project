4454909f65ceba38c13a866c4563d7f3573c6752
weeny-V <48623554+weeny-V@users.noreply.github.com>
2026-06-21 11:46:11 +0300

    Implement search functionality and style SearchPage.

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -7,6 +7,7 @@ import Navbar from './components/Navbar/Navbar';
 import LoginPage from "./components/LoginPage/LoginPage";
 import RegisterPage from "./components/RegisterPage/RegisterPage";
 import DetailsPage from "./components/DetailsPage/DetailsPage";
+import SearchPage from "./components/SearchPage/SearchPage";
 
 function App() {
 
@@ -19,6 +20,7 @@ function App() {
           <Route path="/app/login" element={<LoginPage />} />
           <Route path="/app/register" element={<RegisterPage />} />
           <Route path="/app/product/:productId" element={<DetailsPage/>} />
+          <Route path="/app/search" element={<SearchPage/>} />
         </Routes>
         </>
   );
--- giftlink-frontend/src/components/Navbar/Navbar.js
+++ giftlink-frontend/src/components/Navbar/Navbar.js
@@ -1,17 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="/">GiftLink</a>
+            <Link className="navbar-brand" href="/">GiftLink</Link>
 
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
                     <li className="nav-item">
-                        <a className="nav-link" href="/home.html">Home</a>
+                        <Link className="nav-link" href="/home.html">Home</Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="/app">Gifts</a>
+                        <Link className="nav-link" href="/app">Gifts</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/app/search">Search</Link>
                     </li>
 
                 </ul>
