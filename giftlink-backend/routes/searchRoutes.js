922a4b205d531ff0493fae76e161050df706452a
weeny-V <48623554+weeny-V@users.noreply.github.com>
2026-06-22 12:16:29 +0300

    lint

--- giftlink-backend/routes/searchRoutes.js
+++ giftlink-backend/routes/searchRoutes.js
@@ -1,3 +1,4 @@
+/*jshint esversion: 8 */
 const express = require('express');
 const router = express.Router();
 const connectToDatabase = require('../models/db');
@@ -30,7 +31,7 @@ router.get('/', async (req, res, next) => {
         }
 
         // Task 4: Fetch filtered gifts using the find(query) method. Make sure to use await and store the result in the `gifts` constant
-        const gifts = await collection.find(query).toArray()
+        const gifts = await collection.find(query).toArray();
 
         res.json(gifts);
     } catch (e) {
